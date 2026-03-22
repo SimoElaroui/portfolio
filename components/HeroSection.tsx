50c56af5d4afa4abe15c1e04a359cba8140bc6d1
Simoo <simomohadiv@gmail.com>
2026-03-22 22:52:52 +0100

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,7 +9,8 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-blue-400 mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-500">
-              Hello, I'm{' '}
+              {/* قمنا بتغيير طريقة كتابة النص هنا لتجنب خطأ ESLint */}
+              Hello, I&apos;m{' '}
             </span>
             <br />
             <TypeAnimation 
