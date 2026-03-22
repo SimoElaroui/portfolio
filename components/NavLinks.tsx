4f893bc87048ff81366a63b4470cfa525fa4da60
Simoo <simomohadiv@gmail.com>
2026-03-22 23:17:09 +0100

    Add optional onClick prop to NavLinks
    Added optional onClick prop to NavLinks component.

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
+// قمنا بإضافة onClick كخاصية اختيارية (?) لتجنب أي تعارض
 interface NavLinkProps {
   href: string;
   title: string;
+  onClick?: () => void; 
 }
 
-const NavLinks: React.FC<NavLinkProps> = ({ href, title }) => {
+const NavLinks: React.FC<NavLinkProps> = ({ href, title, onClick }) => {
   return (
     <Link
       href={href}
+      onClick={onClick} // تأكد من تمريرها هنا ليتم تنفيذها عند الضغط
       className="block py-2 pl-3 pr-4 text-white font-weight prociono-regular sm:text-xl rounded md:p-0 hover:text-blue-600"
     >
       {title}
